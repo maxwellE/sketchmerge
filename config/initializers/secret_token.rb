2e0d38462144949d3ed00651e2d820bb52eca2da
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2012-12-17 00:00:35 -0500

    started views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SketchMerge::Application.config.secret_token = 'f4954fc7586d8bed52f98e61672b9f28de9127ceadf22e2ebc1e9d9d22c8365a9223beabcbdf844fa564a48592df92a23c6b0d7b8b8d62ff3595ebd0ec20bb8d'
