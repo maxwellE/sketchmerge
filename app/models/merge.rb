c60fba0631f8d661b6b5220db590c9af62f10a4d
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2013-01-18 19:14:28 -0500

    no database.yml

--- app/models/merge.rb
+++ app/models/merge.rb
@@ -3,21 +3,21 @@ class Merge < ActiveRecord::Base
   belongs_to :receiver, :class_name => "User", :foreign_key => "receiver_id"
   attr_accessible :pending, :user_id, :receiver_id
 
-  def self.generate_create_json(current_user,params)
-    if params[:to_username] != current_user.username
-      if User.exists?(:username => params[:to_username])
-        if current_user.merges.exists?(:receiver_id => User.where("username = ?", params[:to_username])) or
-            User.where("username = ?", params[:to_username]).first.merges.exists?(:receiver_id => current_user.id)
-          {:error => "You already have merged with '#{params[:to_username]}'!"}
+  def self.generate_create_json(current_user,to_user)
+    if to_user != current_user.username
+      if User.exists?(:username => to_user)
+        if current_user.merges.exists?(:receiver_id => User.where("username = ?", to_user)) or
+            User.where("username = ?", to_user).first.merges.exists?(:receiver_id => current_user.id)
+          {:error => "You already have merged with '#{to_user}'!"}
         else
-          new_merge = current_user.merges.create(:receiver_id => User.where("username = ?", params[:to_username]).first.id)
+          new_merge = current_user.merges.create(:receiver_id => User.where("username = ?", to_user).first.id)
           merge_json = {}
           merge_json["to_username"] = new_merge.receiver.username
           merge_json["merge_id"] = new_merge.id
           merge_json
         end
       else
-        {:error => "User with username '#{params[:to_username]}' does not exist!"}
+        {:error => "User with username '#{to_user}' does not exist!"}
       end
     else
       {:error => "You cannot add yourself as a merger!"}
